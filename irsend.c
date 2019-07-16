b0885c7381b2e3a7cda386f8f9bcfbadc76a7d57
duodiscus92 <jacques.ehrlich@neuf.fr>
2019-07-16 11:25:33 +0100

    nettoyage des impressions de tests et autres inutilités

--- irsend.c
+++ irsend.c
@@ -6,49 +6,34 @@
 
 #include <wiringPi.h>
 
-#define CODE_SIZE 32
+#define CODE_SIZE 32	// bit number of the key code
 
-#define IR_PIN	22
+#define IR_PIN	22 // GPIO 22
 
-/*
-#define MARK_ONE  580
-#define MARK_ZERO 1600
-#define SPACE 580
-#define HEADER_MARK 8000
-#define HEADER_SPACE 4000
-#define GAP 600000
-#define KEY_PLUS 0x0700DE21
-*/
-
-#define SPACE_ONE 1687 /*1500*/
-#define SPACE_ZERO /*600*/562
-#define MARK /*600*/ /*500*/562
-#define HEADER_MARK 9000
-#define HEADER_SPACE 4500
-#define GAP 60000
+/* duration in ns */
+#define SPACE_ONE 	1687
+#define SPACE_ZERO 	562
+#define MARK 		562
+#define HEADER_MARK 	9000
+#define HEADER_SPACE 	4500
+#define GAP 		60000
 
 #define NB_KEYS		9
 #define KEY_ONOFF	0xF8FFC13E
-#define KEY_MODE	0xF8FF837C /*0X07007C83*/
-#define KEY_FAN		0xF8FF03FC /*0X0700FC03*/
-#define KEY_PLUS 	0xF8FF21DE /*0x0700DE21*/
-#define KEY_MINUS 	0xF8FF01FE /*0x0700FE01*/
-#define KEY_TIMER	0xF8FF817E /*0x07007E81*/
-#define KEY_OSC		0xF8FFA15E /*0x07005EA1*/
+#define KEY_MODE	0xF8FF837C
+#define KEY_FAN		0xF8FF03FC
+#define KEY_PLUS 	0xF8FF21DE
+#define KEY_MINUS 	0xF8FF01FE
+#define KEY_TIMER	0xF8FF817E
+#define KEY_OSC		0xF8FFA15E
 #define KEY_HOME	0xF8FF41BE
-#define KEY_SILENT	0xF8FF619E /*0X07009E61*/
-// code for LG TV (for test purpose)
+#define KEY_SILENT	0xF8FF619E
+// code for my LG TV (for test purpose only)
 #define KEY_LGMA	0x20DF10EF
 #define KEY_LGONE	0x20DF8877
 #define KEY_LGTWO	0x20DF48B7
 
 
-//struct timespec {
-//   time_t tv_sec;        /* seconds */
-//   long   tv_nsec;       /* nanoseconds */
-//} req;
-
-typedef enum {ONOFF, MODE, FAN, PLUS, MINUS, TIMER, OSC, HOME, SILENT} KEYS;
 
 typedef struct {
   char keyname[10];
@@ -77,11 +62,11 @@ typedef struct {
 
 DOMOTICZKEY dkeys[] = {
   0, KEY_ONOFF, 
-  -1, KEY_MODE, // non utilisé en mode domotique
+  -1, KEY_MODE, // not used in Domoticz
   10, KEY_FAN,
   20, KEY_PLUS,
   30, KEY_MINUS,
-  -1, KEY_TIMER, // non utilisé en mode domotique
+  -1, KEY_TIMER, // nont used in Domoticz
   40, KEY_OSC,
   50, KEY_HOME,
   60, KEY_SILENT 
@@ -89,57 +74,44 @@ DOMOTICZKEY dkeys[] = {
 
 struct timespec req;
 
+/* transmission of the code (32 bits) */
 void  sendcode(long code)
 {
    int i;
 
    for(i=0; i<CODE_SIZE; i++){
       if (code < 0){
-         //printf("1");
-         // it is a 1
          digitalWrite(IR_PIN, HIGH);
-         //printf("|");
          req.tv_nsec = MARK*1000;
          nanosleep(&req, NULL);
-         //printf("~");
          digitalWrite(IR_PIN, LOW);
-         //printf("|");
          req.tv_nsec = SPACE_ONE*1000;
          nanosleep(&req, NULL);
-         //printf("_");
       }
       else {
-         //printf("0");
-         // it is a 0
          digitalWrite(IR_PIN, HIGH);
-         //printf("|");
          req.tv_nsec = MARK*1000;
          nanosleep(&req, NULL);
-         //printf("~");
          digitalWrite(IR_PIN, LOW);
-         //printf("|");
          req.tv_nsec = SPACE_ZERO*1000;
          nanosleep(&req, NULL);
-         //printf("__");
       }
       code <<=1 ;
    }
-   //printf("\n");
 }
 
 int main (int argc, char** argv)
 {
-   int repeat, domoticzmode = 0, domoticzcode;
+   int repeat = 0, domoticzmode = 0, domoticzcode;
    char key[10];
    int i, c;
    long code;
 
 
-   /* valeur par défaut des options */
-   repeat = 1;
+   /* default values */
    strcpy(key, "ONOFF");
 
-   /* récupération des options de la ligne de commande */
+   /* gzetting parameters on command call */
    while ((c = getopt(argc , argv, "dk:r:h")) != -1)
       switch (c) {
       case 'd':
@@ -161,12 +133,13 @@ int main (int argc, char** argv)
 
    /* initialisations diverses */
    req.tv_sec = 0;
+   /* WiringPi initialization */
    wiringPiSetupGpio() ;
    pinMode(IR_PIN, OUTPUT);
 
-   /* si on est en mode domoticz on boucle sans fin, sinon c'est one-shot */
+   /* endless loop if in Domoticz mode otherwise on-shot only/si on est en mode domoticz on boucle sans fin, sinon c'est one-shot */
    do{
-      //obtenir le code à émettre (mode manuel)
+      //gettin the code to transmit (manual mode)/obtenir le code à émettre (mode manuel)
       if(domoticzmode == 0){
          for(i = 0; i < NB_KEYS; i++)
             if(!strcmp(key, mkeys[i].keyname)){
@@ -174,11 +147,8 @@ int main (int argc, char** argv)
 	       break;
       	    }
       }
-      //obtenir le code à émettre mode domoticz
+      //getting the code to transmit in Domoticz mode/ obtenir le code à émettre mode Domoticz
       else {/*domoticsmode == 1*/
-	 //fgets(key, 10, stdin);
-	 //fprintf(stderr, "Lecture code domotique sur stdin:%s\n", key);
-	 //domoticzcode = atoi(key);
 	 fscanf(stdin, "%d", &domoticzcode);
 	 fprintf(stderr, "Code domotique reçu : %d\n", domoticzcode);
          for(i = 0; i < NB_KEYS; i++)
@@ -197,7 +167,8 @@ int main (int argc, char** argv)
       }
 
       fprintf(stderr, "Emission du code 0x%08x\n", code);
-      //header
+
+      // transmitting the header
       digitalWrite(IR_PIN, HIGH);
       req.tv_nsec = HEADER_MARK*1000;
       nanosleep(&req, NULL);
@@ -205,16 +176,16 @@ int main (int argc, char** argv)
       req.tv_nsec = HEADER_SPACE*1000;
       nanosleep(&req, NULL);
 
-      /* emettre le code infrarouge */
+      /* transmitting the code itself  */
       sendcode(code);
 
-      //stop
+      /* transmitting the stop bit  */
       digitalWrite(IR_PIN, HIGH);
       req.tv_nsec = MARK*1000;
       nanosleep(&req, NULL);
       digitalWrite(IR_PIN, LOW);
 
-      //repeat code 
+      //repeat code  if requested (option -r)
       usleep(36000);
       for(i=0; i<repeat; i++){
       digitalWrite(IR_PIN, HIGH);
@@ -228,8 +199,7 @@ int main (int argc, char** argv)
       usleep(96190);
       }
 
-      // gap
-      //digitalWrite(IR_PIN, LOW);
+      // gap before tranmitting another code
       req.tv_nsec = GAP*1000;
       nanosleep(&req, NULL);
 
